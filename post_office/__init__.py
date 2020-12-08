f6ad6220f78b886484cc63d1476b6b3ad59a03ab
M-C Pronovost <marie-christine@pop-os.localdomain>
2020-12-08 09:30:44 -0500

    remove overriding post_office

--- post_office/__init__.py
+++
@@ -1,9 +0,0 @@
-from ast import literal_eval
-from os.path import dirname, join
-
-with open(join(dirname(__file__), 'version.txt'), 'r') as fh:
-    VERSION = literal_eval(fh.read())
-
-from .backends import EmailBackend
-
-default_app_config = 'post_office.apps.PostOfficeConfig'
