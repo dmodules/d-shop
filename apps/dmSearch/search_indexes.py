07a2c6764ee6bd4b0ca40cd2f683bcd6e497779b
Prashant Mavadiya <pjmavadiya2008@gmail.com>
2020-12-18 01:59:45 +0530

    Change search index

--- apps/dmSearch/search_indexes.py
+++ apps/dmSearch/search_indexes.py
@@ -5,7 +5,7 @@ from dshop.models import Product
 class ProductIndex(indexes.SearchIndex, indexes.Indexable):
    text = indexes.CharField(document=True, use_template=True)
    product_name = indexes.CharField(model_attr="product_name")
-   get_description = indexes.CharField(model_attr="get_description")
+   description = indexes.CharField(model_attr="description")
    
    def get_model(self):
       return Product
