dcfc6eff15f636f8f10fa0ef81cd46a83550de01
Prashant Mavadiya <pjmavadiya2008@gmail.com>
2020-12-18 02:04:10 +0530

    Haystack remove and add filter search

--- apps/dmSearch/views.py
+++ apps/dmSearch/views.py
@@ -8,12 +8,23 @@ def search_product(request):
     q = request.GET.get('q', '')
     if q is '':
         return JsonResponse({})
-    all_results = SearchQuerySet().filter(content=q)
+    #Temperory don't use this for less products
+    #all_results = SearchQuerySet().filter(content=q)
 
-    products = [result.pk for result in all_results]
-    products = Product.objects.filter(id__in=products)
+    #products = [result.pk for result in all_results]
+    #products = Product.objects.filter(id__in=products)
 
+    #Search from name
+    query1 = Product.objects.filter(product_name__contains=q)
+    q2 = []
+    #Search from description
+    for product in Product.objects.all():
+        if q in product.get_description:
+            q2.append(product.id)
+    query2 = Product.objects.filter(id__in=q2)
+
+    query = query1 | query2
     serializer_context = {'request': request}
-    serializer = ProductSerializer(products, context=serializer_context, many=True)
+    serializer = ProductSerializer(query, context=serializer_context, many=True)
     return JsonResponse(serializer.data, safe=False)
 
