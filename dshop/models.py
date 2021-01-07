eac0a620b7744ad7f4f9023597b4f03d132bb252
M-C Pronovost <mariechristine@d-modules.com>
2021-01-07 12:41:21 -0500

    add options to infolettre plugin

--- dshop/models.py
+++ dshop/models.py
@@ -1163,6 +1163,11 @@ class dmInfolettre(CMSPlugin):
         blank=True,
         help_text=_("Maximum 100 characters.")
     )
+    title_color = ColorField(
+        verbose_name=_("Title's Colour"),
+        null=True,
+        blank=True
+    )
     subtitle = models.CharField(
         verbose_name=_("Subtitle"),
         max_length=200,
@@ -1170,9 +1175,19 @@ class dmInfolettre(CMSPlugin):
         blank=True,
         help_text=_("Maximum 200 characters.")
     )
+    subtitle_color = ColorField(
+        verbose_name=_("Subtitle's Colour"),
+        null=True,
+        blank=True
+    )
     text = HTMLField(
         verbose_name=_("Text"),
         configuration="CKEDITOR_SETTINGS_DMPLUGIN",
+        null=True,
+        blank=True
+    )
+    text_color = ColorField(
+        verbose_name=_("Text's Colour"),
         null=True,
         blank=True
     )
@@ -1189,6 +1204,11 @@ class dmInfolettre(CMSPlugin):
         null=True,
         blank=True,
         help_text=_("Leave blank to hide image.")
+    )
+    bg_color = ColorField(
+        verbose_name=_("Background's Colour"),
+        null=True,
+        blank=True
     )
 
 
