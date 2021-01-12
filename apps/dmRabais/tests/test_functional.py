cf6e5510f078a25734f57b3abd52355343d3528c
Prashant Mavadiya <pjmavadiya2008@gmail.com>
2021-01-12 14:16:30 +0530

    Test case to check price after dmRabaisPerCategory created

--- apps/dmRabais/tests/test_functional.py
+++ apps/dmRabais/tests/test_functional.py
@@ -1,10 +1,14 @@
 
+import pytz
 from django.test import TestCase
 from datetime import datetime, timedelta
 
+from .utils import \
+    create_discount
 from dshop.models import Product
-
-from dshop.utils_test import category, product
+from dshop.utils_test import \
+    category, \
+    product
 
 
 class CategoryDiscountTestProduct(TestCase):
@@ -17,8 +21,8 @@ class CategoryDiscountTestProduct(TestCase):
         prod = Product.objects.all().first()
 
         prod.discounted_price = 50
-        prod.start_date = datetime.today() - timedelta(days=2)
-        prod.end_date = datetime.today() + timedelta(days=2)
+        prod.start_date = pytz.utc.localize(datetime.today() - timedelta(days=2))
+        prod.end_date = pytz.utc.localize(datetime.today() + timedelta(days=2))
         prod.save()
 
         prod = Product.objects.all().first()
@@ -29,10 +33,23 @@ class CategoryDiscountTestProduct(TestCase):
         prod = Product.objects.all().first()
 
         prod.discounted_price = 50
-        prod.start_date = datetime.today() - timedelta(days=2)
-        prod.end_date = datetime.today() - timedelta(days=1)
+        prod.start_date = pytz.utc.localize(datetime.today() - timedelta(days=2))
+        prod.end_date = pytz.utc.localize(datetime.today() - timedelta(days=1))
         prod.save()
 
         prod = Product.objects.all().first()
         new_price = prod.get_price()
         self.assertNotEqual(prod.discounted_price, new_price)
+
+    def test_category_discount(self):
+        prod = Product.objects.all().first()
+        old_price = prod.get_price()
+        amount = 10
+        # Create discount
+        create_discount()
+        # Get new Price
+        new_price = prod.get_price()
+        self.assertEqual(
+            float(old_price.to_eng_string()),
+            float(new_price.to_eng_string()) + amount
+        )
