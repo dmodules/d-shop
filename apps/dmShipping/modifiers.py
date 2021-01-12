25bb79304431fe014524aeb92830d56c6e883c17
M-C Pronovost <mariechristine@d-modules.com>
2021-01-12 11:12:00 -0500

    add Pickup shipping modifier

--- apps/dmShipping/modifiers.py
+++ apps/dmShipping/modifiers.py
@@ -13,6 +13,39 @@ from settings import DEFAULT_FROM_EMAIL
 from .models import ShippingManagement
 
 
+class PickupShippingModifier(ShippingModifier):
+    identifier = "pickup-in-store"
+
+    def get_choice(self):
+        sm = ShippingManagement.objects.filter(
+            identifier=self.identifier).first()
+        if sm:
+            return (self.identifier, sm.name)
+        return (None, None)
+
+    def add_extra_cart_row(self, cart, request):
+        # ===---
+        shipping_modifiers = cart_modifiers_pool.get_shipping_modifiers()
+        if not self.is_active(
+            cart.extra.get("shipping_modifier")
+        ) and len(shipping_modifiers) > 1:
+            return
+        # ===---
+        sm = ShippingManagement.objects.filter(
+            identifier=self.identifier
+        ).first()
+        cp = float(str(cart.subtotal).split(" ")[1].replace(",", "."))
+        if sm:
+            if (sm.use_separator and sm.separator is not None
+                    and sm.price_after is not None and cp >= sm.separator):
+                amount = Money(sm.price_after)
+            else:
+                amount = Money(sm.price)
+            instance = {"label": _("Shipping costs"), "amount": amount}
+            cart.extra_rows[self.identifier] = ExtraCartRow(instance)
+            cart.total += amount
+
+
 class FreeShippingModifier(ShippingModifier):
     identifier = "free-shipping"
 
