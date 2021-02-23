c2fc823ce7759c4bd1cd0def8d5082ad08bc1ab3
Prashant Mavadiya <pjmavadiya2008@gmail.com>
2021-02-23 22:13:32 +0530

    Integrate cities-light

--- apps/dmShipping/views.py
+++ apps/dmShipping/views.py
@@ -6,15 +6,16 @@ from .models import ShippingManagement, \
     ShippingCountry, \
     ShippingState, \
     ShippingCity
+from cities_light.models import Country, Region, City
 from dal import autocomplete
 
 class CountryAutocomplete(autocomplete.Select2QuerySetView):
 
     def get_queryset(self):
         if not self.request.user.is_authenticated:
-            return ShippingCountry.objects.none()
+            return Country.objects.none()
 
-        qs = ShippingCountry.objects.all()
+        qs = Country.objects.all()
 
         if self.q:
             qs = qs.filter(Q(name__istartswith=self.q))
@@ -25,10 +26,10 @@ class StateAutocomplete(autocomplete.Select2QuerySetView):
 
     def get_queryset(self):
         if not self.request.user.is_authenticated:
-            return ShippingState.objects.none()
+            return Region.objects.none()
 
         countries = self.forwarded.get('countries', None)
-        qs = ShippingState.objects.all()
+        qs = Region.objects.all()
 
         if countries:
             qs = qs.filter(country__id__in=countries)
@@ -42,16 +43,16 @@ class CityAutocomplete(autocomplete.Select2QuerySetView):
 
     def get_queryset(self):
         if not self.request.user.is_authenticated:
-            return ShippingCity.objects.none()
+            return City.objects.none()
 
         states = self.forwarded.get('states', None)
-        qs = ShippingCity.objects.all()
+        qs = City.objects.all()
 
         if states:
-            qs = qs.filter(state__id__in=states)
+            qs = qs.filter(region_id__in=states)
 
         if self.q:
-            qs = qs.filter(Q(name__istartswith=self.q) | Q(state__name__istartswith=self.q))
+            qs = qs.filter(Q(name__istartswith=self.q) | Q(region__name__istartswith=self.q))
         return qs
 
 
@@ -84,25 +85,25 @@ def get_data(request):
             sta = list(shipping.states.all().values_list('name', 'code'))
             if not con:
                 for s in sta:
-                    s_obj = ShippingState.objects.get(code=s[1])
-                    con += [(s_obj.country.name, s_obj.country.code)]
+                    s_obj = Region.objects.get(code=s[1])
+                    con += [(s_obj.country.name, s_obj.country.code2)]
         else:
             codes = [ c[1] for c in con]
-            sta = list(ShippingState.objects.filter(country__code__in=codes).values_list('name', 'code'))
+            sta = list(Region.objects.filter(country__code2__in=codes).values_list('name', 'slug'))
 
         if shipping.cities.all():
-            cities  += list(shipping.cities.all().values_list('name', 'code'))
+            cities  += list(shipping.cities.all().values_list('name', 'id'))
             if not sta:
                 for c in cities:
-                    c_obj = ShippingCity.objects.get(code=c[1])
-                    sta += [(c_obj.state.name, c_obj.state.code)]
+                    c_obj = City.objects.get(id=c[1])
+                    sta += [(c_obj.region.name, c_obj.region.slug)]
             if not con:
                 for s in sta:
-                    s_obj = ShippingState.objects.get(code=s[1])
-                    con += [(s_obj.country.name, s_obj.country.code)]
+                    s_obj = Region.objects.get(slug=s[1])
+                    con += [(s_obj.country.name, s_obj.country.code2)]
         else:
             codes = [ c[1] for c in sta]
-            cit = list(ShippingCity.objects.filter(state__code__in=codes).values_list('name', 'code'))
+            cit = list(City.objects.filter(region__slug__in=codes).values_list('name', 'id'))
 
         countries += con
         states += sta
@@ -117,20 +118,20 @@ def get_data(request):
     for country in temp_co:
         temp = {}
         for s in states:
-            st = ShippingState.objects.filter(code=s[1], country__code=country).first()
+            st = Region.objects.filter(slug=s[1], country__code2=country).first()
             if st:
-                if st.code not in temp:
-                    temp[st.code] = st.name
+                if st.slug not in temp:
+                    temp[st.slug] = st.name
         temp_st[country] = temp
 
     temp_ct = {}
     for state in states:
         temp = {}
         for c in cities:
-            ct = ShippingCity.objects.filter(code=c[1], state__code=state[1]).first()
+            ct = City.objects.filter(id=c[1], region__slug=state[1]).first()
             if ct:
-                if ct.code not in temp:
-                    temp[ct.code] = ct.name
+                if ct.id not in temp:
+                    temp[ct.id] = ct.name
         temp_ct[state[1]] = temp
 
     data = {
