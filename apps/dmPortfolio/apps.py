76654efb8ecd6695095010bf053d785b233da452
Martin Cote <martcot@gmail.com>
2021-05-06 14:18:59 -0400

    remove emploi and portfolio module

--- apps/dmPortfolio/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class dmPortfolioConfig(AppConfig):
-    name = "apps.dmPortfolio"
-    verbose_name = _("Portfolio")
