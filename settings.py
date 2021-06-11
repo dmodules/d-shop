579f848738762e49c65de27afe6193ff492c32a0
prashant mavadiya <pjmavadiya2008@gmail.com>
2021-06-11 20:09:55 +0530

    Sitempa fix

--- settings.py
+++ settings.py
@@ -76,6 +76,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     'cms',
     'menus',
     'treebeard',
